79a1f34705fdf73fe06c321ac1d08ab6c5799634
Anis <anistrabelsi.contact@gmail.com>
2026-07-08 19:41:13 +0200

    style: rend l'ensemble de l'application responsive mobile

--- frontend/src/app/layout.js
+++ frontend/src/app/layout.js
@@ -12,6 +12,7 @@ export default function RootLayout({ children }) {
         <html lang="fr" suppressHydrationWarning>
             <head>
                 <meta charSet="utf-8" />
+                <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" />
             </head>
             <body>
                 <I18nProvider>
